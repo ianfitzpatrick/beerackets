66f0260f5cd5447ee401ac888a35e7dea57fe883
ianfitzpatrick1 <ianfitz@gmail.com>
2020-03-12 23:04:11 -0500

    Removed incorrect context processor

--- beerackets/settings/base.py
+++ beerackets/settings/base.py
@@ -71,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'sekizai.context_processors.sekizai'
 
             ],
         },
