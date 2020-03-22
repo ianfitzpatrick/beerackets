6fc83e14176cb11296625b1330d176f7bb3243a8
ianfitzpatrick1 <ianfitz@gmail.com>
2020-03-22 15:46:50 -0500

    Added challenge creation and validation model methods to ladder model

--- ladders/models.py
+++ ladders/models.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from django.db import models
 from leagues.models import League
-
+from matches.models import Match
 
 class Ladder(models.Model):
     league = models.OneToOneField(League, on_delete=models.CASCADE)
@@ -10,3 +10,59 @@ class Ladder(models.Model):
 
     def __str__(self):
         return f'Ladder for {self.league}'
+
+    def create_challenge(self, challenger, defender):
+        """
+        Generate a new match challenge after validating it's possible.
+        """
+        if self.validate_challenge(challenger, defender):
+            match = Match.objects.create(
+                challenge=challenger,
+                defender=defender
+            )
+            return match
+
+        return None
+
+    def validate_challenge(self, challenger, defender):
+        """
+        Confirm a possible challenge match meets required criteria.
+
+        Match challenge criteria are as following:
+
+            - Neither team has an OPEN match challenge pending
+            
+            - Both teams are within ONE rank of each other OR
+              Both teams have SAME rank
+            
+            - Both teams are in the same LEAGUE
+
+            - Both teams are not the SAME team
+        """        
+        # Start with most conservative assumptions
+        open_matches = same_team_twice = True
+        same_league = teams_within_one_rank = teams_have_same_rank = False
+
+        if not challenger.open_match and not defender.open_match:
+            open_matches = False
+
+        if challenger != defender:
+            same_team_twice = False
+
+        if challenger.league == defender.league:
+            same_league = True
+
+        if defender.ladder_rank - challenger.ladder_rank == 1:
+            teams_within_one_rank = True
+
+        if defender.ladder_rank == challenger.ladder_rank:
+            teams_have_same_rank = True
+
+
+        if not open_matches and not same_team_twice:
+            if same_league:
+                if teams_within_one_rank or teams_have_same_rank:
+                    return True
+
+        return False
+
