9910ee21731d17142124b2d3e2a9ac49052d1e40
ianfitzpatrick1 <ianfitz@gmail.com>
2020-03-30 16:39:19 -0500

    Added captains field to league model

--- leagues/models.py
+++ leagues/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class League(models.Model):
     """Organize Teams within a League."""
     name = models.CharField(max_length=255, help_text='Name your league')
+    captains = models.ManyToManyField(User, related_name='captain_of')
 
     modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
